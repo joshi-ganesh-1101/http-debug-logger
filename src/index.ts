25270a6f4f3d5bf86aee6382b1668840f2baab40
Ganesh Joshi <ganeshjoshi583@gmail.com>
2025-05-09 20:08:55 +0000

    fix/express-integration

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
-import { LoggerOptions } from './types/loggerOptions';
-import { initFileStream } from './helpers/initFileStream';
-import { makeOutputLog } from './helpers/makeOutputLog';
-import { attachLogger } from './helpers/attachLogger';
-import { applyPresets } from './helpers/applyPresets';
+import { createServerLogger } from './createServerLogger';
+import { expressLogger } from './express';
 
-export function createServerLogger(options: LoggerOptions = {}) {
-  const opts = applyPresets(options);
-
-  const fileStream = initFileStream(opts);
-
-  const outputLog = makeOutputLog(fileStream);
-
-  return attachLogger(opts, outputLog);
-}
+export { createServerLogger, expressLogger };
